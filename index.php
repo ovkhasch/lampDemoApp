7b63c03df50e186953b355413a51f137eba4f14e
ovkhasch <oleg.v.khaschansky@gmail.com>
2016-12-08 11:51:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -20,25 +20,8 @@
     
 <?php
     
-$ini = parse_ini_file(".user.ini");
+include("db.php");
     
-echo "<h3>Node: ";
-print $ini["node.fqdn"];    
-echo "</h3><br>";    
-    
-$servername = $ini["mysql.default.host"];//"mysql.cloudply.org";
-$username = $ini["mysql.default.user"];//"db_admin";
-$password = $ini["mysql.default.password"];//"mysql_admin_password";
-$dbname = "lamp_demo";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
 // Perform SQL query
 $sql = "SELECT * FROM customers";
 $result = $conn->query($sql);
